a1e60312be60c8dc3349930f3359e293eaf9674c
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-03-30 08:57:56 -0300

    inicio

---
+++ src/main/java/br/com/etec/gabriel/provapw/model/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.etec.gabriel.provapw.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nomeCliente;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id == cliente.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/br/com/etec/gabriel/provapw/ProvapwApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.gabriel.provapw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvapwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
